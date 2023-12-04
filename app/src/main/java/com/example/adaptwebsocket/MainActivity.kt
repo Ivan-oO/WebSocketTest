c0ad71e302b754432b91b27ffa94710487633ef5
Ivan-oO <ivan@adaptframework.solutions>
2023-12-04 14:45:26 -0100

    POC

--- app/src/main/java/com/example/adaptwebsocket/MainActivity.kt
+++ app/src/main/java/com/example/adaptwebsocket/MainActivity.kt
@@ -3,39 +3,48 @@ package com.example.adaptwebsocket
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.adaptwebsocket.ui.theme.AdaptWebSocketTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var webSocketClient: WebSocketClient
+    val webSocketLogs = mutableStateListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AdaptWebSocketTheme {
+                WebSocketLogs(webSocketLogs)
+            }
+        }
 
-        webSocketClient = WebSocketClient()
+        webSocketClient = WebSocketClient { log ->
+            runOnUiThread {
+                webSocketLogs.add(log)
+            }
+        }
         webSocketClient.start()
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun WebSocketLogs(logs: List<String>, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        logs.forEach { log ->
+            Text(text = log)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WebSocketLogsPreview() {
     AdaptWebSocketTheme {
-        Greeting("Android")
+        WebSocketLogs(listOf("Log 1", "Log 2"))
     }
 }
